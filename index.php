ba0c903556c27fe9dd2d42404948bd9e13f4da8f
SUBHADEEP96 <48767231+SUBHADEEP96@users.noreply.github.com>
2019-10-28 12:47:03 +0530

    Landing page

---
+++ index.php
@@ -0,0 +1,406 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<!--googlefont-->
+<link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" /><link href='https://fonts.googleapis.com/css?family=Abril Fatface' rel='stylesheet'>
+<link rel="stylesheet" href="style.css">
+<title>Gobind Sweets || We Welcome You</title>
+
+</head>
+<body>
+
+<!--Navigation-->
+<nav class="navbar navbar-expand-md navbar-light  sticky-top" style="background-color:#eb2f06">
+
+<div class="container-fluid">
+
+<a class="navbar-brand" href="index.php"><img src="img/log.png" class="img-thumbnail img-fluid"></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navresponsive">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navresponsive">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+<h5><a class="nav-link" href="#"><i class="fas fa-home" style="color:white"></i></a></h5>
+</li>
+
+
+<li class="nav-item">
+<a class="nav-link text-white" href="#">About us</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link text-white href="#">Our Chart</a>
+</li>
+
+<li class="nav-item dropdown">
+<a class="nav-link text-white dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+Menu
+
+</a>
+
+<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Dairy</a>
+		  
+		  <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Desserts</a>
+		  
+		  <div class="dropdown-divider"></div>
+		   <a class="dropdown-item" href="#">Appetizers</a>
+		   
+		   <div class="dropdown-divider"></div>
+		   
+		    <a class="dropdown-item" href="#">Mithai</a>
+			 
+          
+        </div>
+</li>
+
+<li class="nav-item">
+<a class="nav-link text-white" href="#">Contact</a>
+</li>
+
+</ul>
+
+
+
+</div>
+
+
+</div>
+
+
+</nav>
+
+
+
+
+<!--Image Slider-->
+
+<div id="slides" class="carousel slide" data-ride="carousel">
+
+<ul class="carousel-indicators">
+
+<li data-target="#slides" data-slide-to="0" class="active"></li>
+<li data-target="#slides" data-slide-to="1"></li>
+<li data-target="#slides" data-slide-to="2"></li>
+
+
+</ul>
+<div class="carousel-inner">
+
+<div class="carousel-item active">
+
+<img src="img/b1.jpg" class="d-block w-100">
+
+<div class="carousel-caption">
+
+
+
+<button type="button" class="btn btn-warning btn-lg" style="font-family: 'Aclonica';">Thomastown</button>
+
+<button type="button" class="btn btn-danger btn-lg" style="font-family: 'Aclonica';">Craigieburn</button>
+
+
+</div>
+
+</div>
+
+<div class="carousel-item">
+<img src="img/b2.jpg" class="d-block w-100">
+
+</div>
+
+<div class="carousel-item">
+<img src="img/b3.jpg" class="d-block w-100">
+<div class="carousel-caption">
+<h3 class="text-primary">Best Indian-Sweets in Australia</h3>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<!--jumbotron-->
+<div class="container-fluid">
+
+<div class="row jumbotron bg-warning">
+
+<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10 ">
+<h2 class="text-center text-white" style="font-family: 'Aclonica';">
+We Have Two Branches of Our Sweet Shop, One is at Thomastown And Another one is At Craigieburn ...
+</h2>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+
+<a href="#"><button type="button" class="btn btn-outline-secondary btn-lg">
+Know More
+</button></a>
+</div>
+
+</div>
+
+</div>
+
+
+<!--Welcome Section-->
+
+<div class="container-fluid padding">
+
+<div class="row welcome text-center">
+
+<div class="col-12">
+<h1 class="display-4" style="font-family: 'Aclonica';">Welcome To Gobind Sweets</h1>
+</div>
+<hr>
+<div class="col-12">
+<p class="lead">
+Gobind Sweets provides you variety of Indian sweets with authentic taste, We take care while making any vegitarian products, taking care of al religious and social sentiments.  
+</p>
+</div>
+
+</div>
+
+</div>
+
+
+<!--Three Column Section-->
+<div class="container-fluid padding">
+<div class="row text-center padding">
+
+<div class="col-xs-12 col-sm-6 col-md-4">
+<img src="img/kaju.jpg" style="width:250px" class="img-fluid" alt="Card image"><br><br>
+<h3>Kaju Barfi</h3>
+</div>
+
+<div class="col-xs-12 col-sm-6 col-md-4">
+<img src="img/cc.png" style="width:250px" class="img-fluid" alt="Card image"><br><br>
+<h3>GulabJamun</h3>
+</div>
+
+<div class="col-xs-12 col-md-4">
+<img src="img/cc1.png" style="width:250px" class="img-fluid" alt="Card image"><br><br>
+<h3>Rasmalai</h3>
+</div>
+
+</div>
+<hr class="my-4">
+</div>
+<!--Two Column Section-->
+<section>
+<div class="container-fluid">
+<h1 class="text-center text-capitalize pt-5">Food Quality</h1>
+<hr class="w-25 mx-auto">
+
+<div class="row mb-4">
+
+<div class="col-lg-6 col-md-6 col-12">
+<img src="img/yog.jpg" style="width:800px;height:500px;";" class="img-fluid">
+
+</div>
+
+
+<div class="col-lg-6 col-md-6 col-12">
+
+<h1 class="text-center">Our Speciality?</h1>
+<p class="pt-4">
+Quality means building the customer�s point of view into every aspect of a Menu from design to final recycling.
+</p>
+<button class="btn btn-primary">Know More</button>
+
+</div>
+
+</div>
+
+</div>
+</section>
+<hr class="my-4">
+<!--Fixed Background-->
+<figure>
+
+<div class="fixed-wrap">
+<div id="fixed">
+<img src="img/yog.jpg" style="width:100%">
+</div>
+</div>
+</figure>
+<!--Menu Section-->
+<section>
+<div class="container-fluid">
+
+<h1 class="text-center text-capitalize pt-5">Menu Category</h1>
+<hr class="w-25 mx-auto">
+
+<div class="row text-center mb-4">
+
+<div class="col-lg-3 col-md-3 col-12">
+<div class="card" style="width:300px">
+  <img class="card-img-top" src="img/kaju.jpg" style="width:300px" class="img-fluid" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title text-center">Dairy</h4>
+  <div class="dropdown">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+   Dairy Items
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Paneer (Fresh Cheese)</a>
+    <a class="dropdown-item" href="#">Lassi (Yogurt Drink)</a>
+    <a class="dropdown-item" href="#">Ghee</a>
+	<a class="dropdown-item" href="#">Dahi</a>
+	<a class="dropdown-item" href="#">Khoa</a>
+	
+  </div>
+</div>
+  </div>
+</div>
+</div>
+
+<div class="col-lg-3 col-md-3 col-12">
+<div class="card" style="width:300px">
+  <img class="card-img-top" src="img/kaju.jpg" style="width:300px" class="img-fluid" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title text-center">Desserts</h4>
+  <div class="dropdown">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+    Dessert Items
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Rasmalai</a>
+    <a class="dropdown-item" href="#">Gulabjamun</a>
+    <a class="dropdown-item" href="#">Rasogolla</a>
+	<a class="dropdown-item" href="#">Moong Daal Halwa</a>
+    <a class="dropdown-item" href="#">Gajar Halwa</a>
+    <a class="dropdown-item" href="#">Angoori Gulabjamun</a>
+	
+  </div>
+</div>
+  
+  </div>
+</div>
+</div>
+
+<div class="col-lg-3 col-md-3 col-12">
+<div class="card" style="width:300px">
+  <img class="card-img-top" src="img/kaju.jpg" style="width:300px"  class="img-fluid" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title text-center">Appetizers</h4>
+    <div class="dropdown">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+    Appetizers Items
+  </button>
+  <div class="dropdown-menu">
+  <a class="dropdown-item" href="#">Aloo Tikka</a>
+    <a class="dropdown-item" href="#">Veggie Burger Patties</a>
+    <a class="dropdown-item" href="#">Veggie Kebabs</a>
+	<a class="dropdown-item" href="#">Spinach Rolls</a>
+    <a class="dropdown-item" href="#">Paneer Pakora</a>
+    <a class="dropdown-item" href="#">Paneer Poppers</a>
+	<a class="dropdown-item" href="#">Hara Bhara Kebab</a>
+    <a class="dropdown-item" href="#">Crunchy Masala Pops</a>
+    <a class="dropdown-item" href="#">Daal Vada</a>
+	<a class="dropdown-item" href="#">Veggie Bites</a>
+    <a class="dropdown-item" href="#">Sweet Chilli Paneer</a>
+    <a class="dropdown-item" href="#">Jalapeno Peppers</a>
+	<a class="dropdown-item" href="#">Batata Wada</a>
+    <a class="dropdown-item" href="#">Corn Kebabs</a>
+    
+  </div>
+</div>
+  </div>
+</div>
+</div>
+<div class="col-lg-3 col-md-3 col-12">
+<div class="card" style="width:300px">
+  <img class="card-img-top" src="img/kaju.jpg" style="width:300px" class="img-fluid" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title text-center">Mithai</h4>
+    <div class="dropdown">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+    Mithai Items
+  </button>
+  <div class="dropdown-menu">
+  <a class="dropdown-item" href="#">Kaju Katli</a>
+    <a class="dropdown-item" href="#">Kaju Roll</a>
+    <a class="dropdown-item" href="#">Milk Cake</a>
+	<a class="dropdown-item" href="#">Kaju Peda</a>
+    <a class="dropdown-item" href="#">Kalakand</a>
+    <a class="dropdown-item" href="#">Mawa Burfi</a>
+	<a class="dropdown-item" href="#">Besan Ladoo</a>
+    <a class="dropdown-item" href="#">Coconut Burfi</a>
+    <a class="dropdown-item" href="#">Bulk Mithai</a>
+	
+  </div>
+</div>
+  </div>
+</div>
+</div>
+</div>
+</section>
+
+</div>
+
+</div>
+
+<!--cards-->
+<div class="container-fluid padding">
+<div class="row padding">
+
+
+</div>
+</div>
+<!--Thomastown Location-->
+<section>
+<div class="container-fluid">
+<h1 class="text-center text-capitalize pt-5">~ Thomastown Location ~</h1>
+<hr class="w-25 mx-auto">
+
+<section>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3157.3306800994187!2d145.01804711505744!3d-37.68843233465464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad64f6eea2b66e5%3A0x8543b14485f070ea!2sUnit%206%2F350%20Settlement%20Rd%2C%20Thomastown%20VIC%203074%2C%20Australia!5e0!3m2!1sen!2sin!4v1572244936780!5m2!1sen!2sin" width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+
+</section>
+</div>
+</section>
+<hr class="my-4">
+<!--Craigieburn Location-->
+<section>
+<div class="container-fluid">
+<h1 class="text-center text-capitalize pt-5">~ Craigieburn Location ~</h1>
+<hr class="w-25 mx-auto">
+
+<section>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3162.2878441033604!2d144.90751401505324!3d-37.57183912801111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad653c9a595cb87%3A0x219de44d15a4113a!2s4%2F439%20Grand%20Blvd%2C%20Craigieburn%20VIC%203064%2C%20Australia!5e0!3m2!1sen!2sin!4v1572244983542!5m2!1sen!2sin" width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+
+
+</section>
+</div>
+</section>
+<hr class="my-4">
+<!--Connect-->
+
+
+<!--Footer-->
+
+</body>
+</html>
